ab589e22dab1b22ffdbfe5d273274c42832974bc
MrPoco2002 <godcraft1380@gmail.com>
2025-11-18 12:44:52 +0330

    refactor : learning : % define

--- learning.py
+++ learning.py
@@ -512,3 +512,13 @@ w = q.json()["data"]["url"]
 if w != w :
     print(s)
 
+# define in strings jadi 23
+
+w = "life"
+e = 1231415907324.23498
+r = 1470
+q = "mohammad is revolved in his life now nad he dosn't know about %s and others life" % w
+qq = "alireza want to be happy now, he haves %i dollars and is not happy" % r
+qqq = "%f peopls lived in this world now" %e
+print(q, qq, qqq)
+
